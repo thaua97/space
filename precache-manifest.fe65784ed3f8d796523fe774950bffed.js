a93ef03c01d4a411b794563c616ef6f0b6c3ed15
Thauã Borges <thaua97@gmail.com>
2019-04-15 17:36:59 -0300

    Updates

---
+++ precache-manifest.fe65784ed3f8d796523fe774950bffed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36073ae66f44a824e322edfa9403a7ec",
+    "url": "/space/static/media/trip.36073ae6.png"
+  },
+  {
+    "revision": "7c290095d35226f6463b37e9160fd59c",
+    "url": "/space/static/media/app.7c290095.png"
+  },
+  {
+    "revision": "918def549b4128417576",
+    "url": "/space/static/js/runtime~main.d884ac96.js"
+  },
+  {
+    "revision": "77cd9f4280d0d1b08c85",
+    "url": "/space/static/js/main.a1a3e863.chunk.js"
+  },
+  {
+    "revision": "975cfae812aff9d95ab7",
+    "url": "/space/static/js/2.fb2f04c1.chunk.js"
+  },
+  {
+    "revision": "a8447981b61aa7e42fec74b379a08888",
+    "url": "/space/index.html"
+  }
+];
